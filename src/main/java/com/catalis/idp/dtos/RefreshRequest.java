b6390b0154719ba0fed3ea6875f416f47c976bd4
Carlos Sánchez <casc84ab@gmail.com>
2025-08-21 09:44:11 +0200

    Add `@JsonProperty` to `RefreshRequest.refreshToken` for JSON serialization

--- src/main/java/com/catalis/idp/dtos/RefreshRequest.java
+++ src/main/java/com/catalis/idp/dtos/RefreshRequest.java
@@ -1,5 +1,6 @@
 package com.catalis.idp.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RefreshRequest {
+    @JsonProperty("refresh_token")
     private String refreshToken;
 }
